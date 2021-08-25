b92bc29c34de9de14c8e480af1c66ba73f2de39e
QuarticCat <QuarticCat@protonmail.com>
2021-08-25 21:33:39 +0800

    add DeferredInit

---
+++ include/rusty-cpp/impl/deferred_init.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+namespace rc {
+
+/// A compromise version of `rc::MaybeUninit`.
+///
+/// Instead of assuming user may not initialize the object, it assumes user will initialize the
+/// object before it destructs.
+///
+/// # Safety
+///
+/// The object must be initialized before it destructs.
+///
+/// # Example
+///
+/// ```
+/// rc::DeferredInit<T> x{};
+/// new (&x) T(args...);
+/// T& inited_x = x;
+/// ```
+template<class T>
+class DeferredInit {
+  private:
+    union {
+        T obj;
+    };
+
+  public:
+    DeferredInit() {}
+
+    DeferredInit(T obj): obj(std::move(obj)) {}
+
+    // TODO: How to deal with move/copy?
+    DeferredInit(const DeferredInit&) = delete;
+    DeferredInit(DeferredInit&&) = delete;
+    DeferredInit& operator=(const DeferredInit&) = delete;
+    DeferredInit& operator=(DeferredInit&&) = delete;
+
+    ~DeferredInit() {
+        obj.~T();
+    }
+
+    operator T&() {
+        return obj;
+    }
+};
+
+}  // namespace rc
--- include/rusty-cpp/rusty-cpp.hpp
+++ include/rusty-cpp/rusty-cpp.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
+#include "impl/deferred_init.hpp"
 #include "impl/maybe_uninit.hpp"
 #include "impl/primitive_types.hpp"
