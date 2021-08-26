5a509e8dc59254e152910d5b07723c3a63c16690
QuarticCat <QuarticCat@protonmail.com>
2021-08-26 21:27:30 +0800

    fix include

--- include/rusty-cpp/rusty-cpp.hpp
+++ include/rusty-cpp/rusty-cpp.hpp
@@ -3,5 +3,6 @@
 // TODO: Modularize it when GCC or Clang has full support for C++20 Module
 
 #include "impl/deferred_init.hpp"
+#include "impl/endian.hpp"
 #include "impl/maybe_uninit.hpp"
 #include "impl/primitive_types.hpp"
